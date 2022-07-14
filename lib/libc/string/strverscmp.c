7f73c2c161a2e0452c20e8a78d1ee2dc0ee902b8
obiwac <obiwac@gmail.com>
2022-07-14 14:17:32 +0200

    strverscmp: Integrate feedback from danfe@ and pauamma@gundo.com

--- lib/libc/string/strverscmp.c
+++ lib/libc/string/strverscmp.c
@@ -9,7 +9,10 @@
 int
 strverscmp(const char *s1, const char *s2)
 {
-	/* if pointers are aliased, no need to go through to process of comparing them */
+	/*
+	 * If pointers are aliased, no need to go through to process of
+	 * comparing them
+	 */
 
 	if (s1 == s2)
 		return 0;
@@ -17,83 +20,87 @@ strverscmp(const char *s1, const char *s2)
 	const unsigned char *u1 = (const void*) s1;
 	const unsigned char *u2 = (const void*) s2;
 
-	for (; *u1 && *u2; u1++, u2++) {
-		/* leading zeros; we're dealing with the fractional part of a number */
+	while (*u1 && *u2) {
+		/* If either character is not a digit, act like strcmp(3) */
 
-		if (*u1 == '0' || *u2 == '0') {
-			/* count leading zeros (more zeros == smaller number) */
+		if (!isdigit(*u1) || !isdigit(*u2)) {
+			if (*u1 != *u2)
+				return (*u1 - *u2);
 
-			size_t n1 = 0;
-			size_t n2 = 0;
+			u1++;
+			u2++;
 
-			for (; *u1 == '0'; u1++)
-				n1++;
-
-			for (; *u2 == '0'; u2++)
-				n2++;
-
-			if (n1 != n2)
-				return (n2 - n1);
-
-			/* handle the case where 000 < 09 */
-
-			if (*u1 == '\0' && isdigit(*u2))
-				return (1);
-
-			if (*u2 == '\0' && isdigit(*u1))
-				return (-1);
-
-			/* for all other cases, compare each digit until there are none left */
-
-			for (; isdigit(*u1) && isdigit(*u2); u1++, u2++) {
-				if (*u1 != *u2)
-					return (*u1 - *u2);
-			}
-
-			u1--;
-			u2--;
-
-			/* no leading zeros; we're simply comparing two numbers */
-
-		} else if (isdigit(*u1) && isdigit(*u2)) {
-			const unsigned char *o1 = u1;
-			const unsigned char *o2 = u2;
-
-			/* count digits (more digits == larger number) */
-
-			size_t n1 = 0;
-			size_t n2 = 0;
-
-			for (; isdigit(*u1); u1++)
-				n1++;
-
-			for (; isdigit(*u2); u2++)
-				n2++;
-
-			if (n1 != n2)
-				return (n1 - n2);
-
-			/*
-			 * if there're the same number of digits,
-			 * go back and compare each digit until there are none left
-			 */
-
-			u1 = o1;
-			u2 = o2;
-
-			for (; isdigit(*u1) && isdigit(*u2); u1++, u2++) {
-				if (*u1 != *u2)
-					return (*u1 - *u2);
-			}
-
-			u1--;
-			u2--;
+			continue;
 		}
 
-		/* for the rest, we can just fallback to a regular strcmp */
+		if (*u1 == '0' || *u2 == '0') {
+			/*
+			 * Leading zeros; we're dealing with the fractional part
+			 * of a number. This is the case even without a decimal
+			 * point. First, count leading zeros (more zeros ==
+			 * smaller number).
+			 */
 
-		if (*u1 != *u2)
-			return (*u1 - *u2);
+			size_t zeros_count_1 = 0;
+			size_t zeros_count_2 = 0;
+
+			for (; *u1 == '0'; u1++)
+				zeros_count_1++;
+
+			for (; *u2 == '0'; u2++)
+				zeros_count_2++;
+
+			if (zeros_count_1 != zeros_count_2)
+				return (zeros_count_2 - zeros_count_1);
+
+			/* Handle the case where 000 < 09. */
+
+			if (!isdigit(*u1) && isdigit(*u2))
+				return (1);
+
+			if (!isdigit(*u2) && isdigit(*u1))
+				return (-1);
+
+		} else {
+			/*
+			 * No leading zeros; we're simply comparing two numbers.
+			 * It is necessary to first count how many digits there
+			 * are before going back to compare each digit, so that
+			 * e.g. 7 is not considered larger than 60.
+			 */
+
+			const unsigned char *num_1 = u1;
+			const unsigned char *num_2 = u2;
+
+			/* Count digits (more digits == larger number). */
+
+			size_t digit_count_1 = 0;
+			size_t digit_count_2 = 0;
+
+			for (; isdigit(*u1); u1++)
+				digit_count_1++;
+
+			for (; isdigit(*u2); u2++)
+				digit_count_2++;
+
+			if (digit_count_1 != digit_count_2)
+				return (digit_count_1 - digit_count_2);
+
+			/*
+			 * If there are the same number of digits, go back to
+			 * the start of the number.
+			 */
+
+			u1 = num_1;
+			u2 = num_2;
+		}
+
+		/* Compare each digit until there are none left. */
+
+		for (; isdigit(*u1) && isdigit(*u2); u1++, u2++) {
+			if (*u1 != *u2)
+				return (*u1 - *u2);
+		}
 	}
 
 	return (*u1 - *u2);
